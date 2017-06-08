5fa8f1e6863d1b463356d17a08e503d16cce0cac
Isabel <isabel_bicalho@yahoo.com.br>
2017-06-08 16:23:27 -0300

    Initial commit

---
+++ index.py
@@ -0,0 +1,28 @@
+import os
+import requests
+import traceback
+import json
+from flask import Flask, request
+
+
+token = os.environ.get('FB_ACCESS_TOKEN')
+app = Flask(__name__)
+@app.route('/', methods=['GET', 'POST'])
+def webhook():
+    if request.method == 'POST':
+        try:
+            data = json.loads(request.data.decode())
+            text = data['entry'][0]['messaging'][0]['message']['text']
+            sender = data['entry'][0]['messaging'][0]['sender']['id']
+            payload = {'recipient': {'id': sender}, 'message': {'text': 'Hello world! :)'}}
+            r = requests.post('https://graph.facebook.com/v2.6/me/messages/?access_token=' + token, json=payload)
+        except Exception as e:
+            print(traceback.format_exc())
+    elif request.method == 'GET': # Para a verificacao inicial
+        if request.args.get('hub.verify_token') == os.environ.get('FB_VERIFY_TOKEN'):
+            return request.args.get('hub.challenge')
+        return "Wrong Verify Token"
+    return 'Nothing'
+
+if __name__ == '__main__':
+    app.run(debug=True)
