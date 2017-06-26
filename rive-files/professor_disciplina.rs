79c2c9bc34ae83133b32955c58ee2ce2d76ea6e7
Isabel <isabel_bicalho@yahoo.com.br>
2017-06-26 19:01:32 -0300

    test

---
+++ rive-files/professor_disciplina.rs
@@ -0,0 +1,16 @@
+! array visualizacao = (dcc030|visualizacao de dados)
+
+! array ihc = (dcc194|ihc|interacao humano computador|interacao humanocomputador)
+
+! array bioinfo = (dcc186|bioinformatica|bioinfo)
+
+! array prof = (prof|profa|professor|professora)
+
++ [*] (@prof) [*] (@ihc) [*]
+- Raquel Oliveira Prates ministra aulas de Interação Humano-Computador.
+
++ [*] (@prof) [*] (@visualizacao) [*]
+- Raquel C. de Melo Minardi ministra aulas de Visualização de Dados.
+
++ [*] (@prof) [*] (@bioinfo) [*]
+- Marcos Augusto dos Santos ministra aulas de Algoritmos Para Bioinformática I e II.
