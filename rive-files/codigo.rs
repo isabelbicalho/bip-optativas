bf0bfa6e4431d5c14993593937ed59bb6b960e66
Isabel <isabel_bicalho@yahoo.com.br>
2017-06-26 18:47:44 -0300

    test

--- rive-files/codigo.rs
+++ rive-files/codigo.rs
@@ -2,7 +2,7 @@
 - O código para Algorimos para Bioinformática I é DCC186 e o 
 ^ código para Algorimos para Bioinformática II é DCC187.
 
-+ [*] codigo [*] interacao (humano-computador|humano computador) [*]
++ [*] codigo [*] interacao (humanocomputador|humano computador) [*]
 - O código para Interação Humano-Computador é DCC194.
 
 + [*] codigo [*] visualizacao de dados [*]
