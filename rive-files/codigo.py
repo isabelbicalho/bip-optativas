6ad5b99a90b4f97311e478fa807d607e6340bf13
Isabel <isabel_bicalho@yahoo.com.br>
2017-06-26 18:38:55 -0300

    Add mothe things

---
+++ rive-files/codigo.py
@@ -0,0 +1,10 @@
++ [*] codigo [*] bioinformatica [*]
+- O código para Algorimos para Bioinformática I é DCC186 e o 
+^ código para Algorimos para Bioinformática II é DCC187.
+
++ [*] codigo [*] interacao (humano-computador|humano computador) [*]
+- O código para Interação Humano-Computador é DCC194.
+
++ [*] codigo [*] visualizacao de dados [*]
+- A disciplina Visualização de Dados se encontra em Tópicos em Ciência 
+^ da Computação (DCC030)
