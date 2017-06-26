6bab7f521bbbadda27a85eb3a7f6d8b6529bf952
Isabel <isabel_bicalho@yahoo.com.br>
2017-06-26 19:17:32 -0300

    test

---
+++ rive-files/ementa.rs
@@ -0,0 +1,48 @@
+! array visualizacao = (dcc030|visualizacao de dados)
+
+! array ihc = (dcc194|ihc|interacao humano computador|interacao humanocomputador)
+
+! array bioinfo = (dcc186|dcc187|bioinformatica|bioinfo)
+
+! array ementa = (ementa|ementas|conteudo|descricao)
+
++ [*] (@ementa) [*] (@visualizacao) [*]
+- Visualização de Dados introduz a base teórica de visualização de informação, 
+^ incluindo princípios de cognição visual e percepção, análise multivariada, 
+^ técnicas de análise visual, princípios de projeto de interfaces visuais e 
+^ aplicações de visualização de informação. Trabalharemos modelos, algoritmos 
+^ e tecnologias ação envolvidos no desenvolvimento de técnicas de visualização. 
+^ Apresentaremos algumas das mais populares metáforas visuais e discutiremos 
+^ suas características e aplicações.
+
++ [*] (@ementa) [*] (@ihc) [*]
+- Interação Humano-Computador apresenta uma visão geral da área de Interação 
+^ Humano-Computador (IHC). Será dada uma mais instrumental e aplicada ao aluno 
+^ tanto de métodos de avaliação, quanto de projeto. Porém a avaliação de sistemas 
+^ interativos será priorizada (em relação a projetos), para que os alunos possam 
+^ apreciar a qualidade do sistema, e entender os requisitos básicos necessários 
+^ para a qualidade do projeto de uma interface. Ementa resumida: A área de IHC 
+^ no Brasil e no Mundo, estilos de interação, qualidades de uso. Processo de 
+^ design na perspectiva de IHC. Métodos de coleta de dados dos usuários. 
+^ Bases teóricas da área de IHC. Métodos de avaliação de base empírica e de 
+^ base teórica. Modelos de projeto de interação.
+
++ [*] (@ementa) [*] (@bioinfo) [*]
+- Algotimos para Bioinformática I tem uma programação aberta, que explora o 
+^ desenvolvimento de algoritmos e modelos para recuperação inteligente de 
+^ informação, utilizando recursos de álgebra linear computacional e 
+^ estatística. Congrega alunos do programa de pós-graduação em Bioinformática 
+^ do ICB e utiliza a sinergia existente entre os discentes de graduação e 
+^ pós-graduação em Sistemas de Informação, Ciência da Computação e 
+^ Matemática Computacional. Não existe pré-requisito. O ambiente de prototipagem 
+^ utilizado é o Matlab, que é explorado em aulas práticas. Ao final do curso, 
+^ as equipes, em geral, expõem os seus trabalhos em congressos da área, 
+^ notadamente o x-Meeting. Alguns trabalhos desenvolvidos neste curso têm 
+^ gerado resultados que, posteriormente, na disciplina Algoritmos para 
+^ Bioinformática II, são preparados para publicação em periódicos da área.
+^ Em Algoritmos para Bioinformática II, o objetivo da disciplina é 
+^ explorar aplicações na área de Programação Matemática e Recuperação 
+^ de Informação (álgebra linear, otimização, análise numérica, estatística) 
+^ envolvendo problemas de biologia molecular. Em geral, os tópicos são definidos 
+^ em função dos problemas explorados pelos alunos em Algoritmos para 
+^ Bioinformática I.
