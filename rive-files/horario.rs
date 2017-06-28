2986a7fda7385333d63c711bb633ff544c1c2068
Isabel <isabel_bicalho@yahoo.com.br>
2017-06-28 16:39:25 -0300

    test

---
+++ rive-files/horario.rs
@@ -0,0 +1,16 @@
+! array visualizacao = (dcc030|visualizacao de dados)
+
+! array ihc = (dcc194|ihc|interacao humano computador|interacao humanocomputador)
+
+! array bioinfo = (dcc186|dcc187|bioinformatica|bioinfo)
+
+! array horario = (hora|horario|dia|dias)
+
++ [*] (@horario) [*] (@visualizacao) [*]
+- Visualização de Dados: seg e qua, 11:10-12:50. 
+
++ [*] (@horario) [*] (@ihc) [*]
+- Interação Humano-Computador: ter e qui, 17:00-18:40. 
+
++ [*] (@horario) [*] (@bioinfo) [*]
+- Algoritmos para Bioinformática ter e qui, 11:10-12:50.
